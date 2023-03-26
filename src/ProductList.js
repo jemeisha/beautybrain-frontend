ea962b538e39623efd52027759db72e0ead91bad
jemeisha <jemeishakithu@gmail.com>
2023-03-26 22:04:19 +0530

    search

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,6 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Input, InputGroup } from "react-daisyui";
 import { useQuery } from "react-query";
+import { useDebounce } from "use-debounce";
+import capitalize from "capitalize";
 
 const PRODUCTS = [
   {
@@ -25,45 +27,84 @@ const PRODUCTS = [
   }
 ]
 
-function Product({ name, url }) {
+function Product({ name, url, brand }) {
   return (
-    <div className="flex flex-col hover:scale-105 cursor-pointer">
-      <img src={url} className="w-52 h-52 rounded-lg" />
-      <div className="mx-auto text-[#1D201F]">{name}</div>
+    <div className="card w-72 bg-[#e7d9ca] shadow-xl overflow-hidden text-black">
+      <div className="w-full h-52 bg-white">
+        <figure><img src={url} alt="Shoes" className="w-full h-52 object-contain" /></figure>
+      </div>
+      <div className="card-body p-2 px-3 pb-3 ">
+        <h2 className="card-title">
+          {capitalize.words(brand)}
+          <div className="badge badge-accent">Rating</div>
+        </h2>
+        <p className="text-sm">{capitalize.words(name)}</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">Fashion</div>
+          <div className="badge badge-outline">Products</div>
+        </div>
+        <div className="flex flex-row">
+
+          {/* <div className="card-actions justify-end mt-5">
+            <button className="btn btn-sm btn-secondary">View Now</button>
+          </div> */}
+        </div>
+      </div>
     </div>
+    // <div className="flex flex-col hover:scale-105 cursor-pointer">
+    //   <img src={url} className="w-52 h-52 rounded-lg" />
+    //   <div className="mx-auto text-[#1D201F]">{name}</div>
+    // </div>
   )
 }
 function ProductList() {
-
-  const { isLoading, error, data } = useQuery('repoData', () =>
-    fetch(`http://127.0.0.1:8000/search/mini`).then(res =>
+  const [query, setQuery] = useState("")
+  const [debouncedQuery] = useDebounce(query, 800)
+  const { isLoading, error, data } = useQuery(['productSearch', debouncedQuery], (data) =>
+    fetch(`http://127.0.0.1:8000/search/${debouncedQuery == "" ? "all_products" : debouncedQuery}`).then(res =>
       res.json()
     )
   )
-  useEffect(() => { 
-   console.log(data)
+  const [jsonData, setJsonData] = useState([])
+  useEffect(() => {
+    console.log(JSON.parse(data || "{}"))
+    setJsonData(JSON.parse(data || "[]"))
+    // console.log(typeof data)
   }, [data])
 
   return (
     <div className="mx-auto flex flex-col">
       <div className="ml-auto mb-8">
         <InputGroup color="accent" >
-          <Input className="bg-white" type="text" placeholder="Search product" bordered />
+          <Input className="bg-white" type="text" placeholder="Search product" bordered onChange={(e) => setQuery(e.target.value)} value={query} />
           <button className="btn btn-square bg-white text-black border-none border-l-2 hover:bg-[#ebdcca]">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
           </button>
         </InputGroup>
       </div>
-      {isLoading && <div>Searching the products...</div>}
+      {isLoading && <div className="mx-auto">Searching the products...</div>}
 
-      {!isLoading && <div className="flex flex-row flex-wrap gap-10 mx-auto">
-        {
+      {!isLoading && <div className="grid grid-cols-4 gap-10 mx-auto justify-around">
+
+        {/* {
           PRODUCTS.map((p) => {
             return <Product name={p.name} url={p.url} />
           })
+        } */}
+        {
+          jsonData.slice(0, 10).map((product) => <Product name={product.name} url={product.img} brand={product.brand} />)
         }
 
       </div>}
+      <div className="my-10 flex flex-row">
+        <div className="btn-group mx-auto text-black">
+          <button className="btn bg-transparent border-0 hover:bg-[#ebdcca] text-black">1</button>
+          <button className="btn bg-transparent border-0 hover:bg-[#ebdcca] text-black">2</button>
+          <button className="btn btn-disabled bg-transparent border-0 hover:bg-[#ebdcca] text-black">...</button>
+          <button className="btn bg-transparent border-0 hover:bg-[#ebdcca] text-black">99</button>
+          <button className="btn bg-transparent border-0 hover:bg-[#ebdcca] text-black">100</button>
+        </div>
+      </div>
     </div>
   )
 }
